e7e13f56edb277510e6fc9947a8a1304ae5cd457
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-09-11 15:41:49 +0530

    Navbar

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import { Header } from '../components';
+import { Header, Navbar } from '../components';
 function HomeLayout() {
   return (
     <>
      <Header/>
+     <Navbar/>
     <section className='align-element py-20'>
      <Outlet />
      </section>
