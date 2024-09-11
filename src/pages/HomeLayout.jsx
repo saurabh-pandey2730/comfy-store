7afc68b5bb693e089dd7ac6c7519d57922b0b4b4
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-09-11 15:33:48 +0530

    Header completed

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import { Header } from '../components';
 function HomeLayout() {
   return (
     <>
-    <nav>
-      <span className='text-4xl text-primary'>Comfy</span>
-    </nav>
-    <Outlet />
+     <Header/>
+    <section className='align-element py-20'>
+     <Outlet />
+     </section>
   </>
   )
 }
