4b45360eae39e16c5177ef2e231798984c690e43
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-09-14 11:49:37 +0530

    hero and about page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 function Landing() {
   return (
     <div>
-       <h1>landing</h1>
+       
+       <Hero/>
     </div>
   )
 }
